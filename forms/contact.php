48f192deae060770b813763c611ac1ed1442301b
rickyaranas <aranasricky@gmail.com>
2023-07-13 10:15:00 +0800

    first commit

---
+++ forms/contact.php
@@ -0,0 +1,68 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'justinlomodcorpuz0626@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'www.google.com',
+    'username' => 'justinlomodcorpuz0626@gmail.com',
+    'password' => 'JustinCorpuz_11',
+    'port' => '587'
+  );
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+
+  // Connect to the MySQL database
+$servername = 'localhost';
+$username = 'your_username';
+$password = 'your_password';
+$dbname = 'contact';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+// Prepare and execute the INSERT statement
+$stmt = $conn->prepare("INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $name, $email, $message);
+
+if ($stmt->execute()) {
+    echo 'Data inserted successfully.';
+} else {
+    echo 'Error inserting data: ' . $stmt->error;
+}
+
+// Close the database connection
+$stmt->close();
+$conn->close();
+?>
